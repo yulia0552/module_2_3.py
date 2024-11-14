1794cc30fae1c4248b9ac8d356e210a6a5ee4b68
Юлия <veberulia530@gmail.com>
2024-11-14 15:52:37 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,24 @@
+my_list_ =[42,69,322,13,0,99,-5,9,8,7,-6,5]
+index = 0
+
+while my_list_[index] >= 0:
+    if my_list_[index] == 0:
+        index += 1
+        continue
+
+    print(my_list_[index])
+    index +=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
